69183b2a1b5247bcb5585d633153804e5236ac24
Muhammad Fadila <muh.fadila18@gmail.com>
2022-10-28 16:04:17 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        // array to be searched
+        int[] arr = new int[20];
+        // number of element in the array
+        int n;
+        // get the number of the element to store in the array
+        int i;
+
+        public void input()
+        {
+            while(true)
+            {
+                Console.Write("Enter the number of the elements in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine
+            }
+        }
+    }
+}
