326a97614a66e09ff09db1135147f8d588b7fede
Muhammad Fadila <muh.fadila18@gmail.com>
2022-10-28 16:07:09 +0700

    add console writeline else

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace SearchAlgorithm
                 n = Int32.Parse(s);
                 if ((n > 0) && (n <= 20))
                     break;
-                
+                else
+                    Console.WriteLine("\n array should have minimum 1 and maximum 20 elements.\n");
                 
             }
         }
